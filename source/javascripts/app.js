c04d495fa5b83978c89a233ae05b012dedb05da3
Blake Tidwell <batidwell@gmail.com>
2018-04-29 20:57:19 -0700

    Disable GA and social media for now

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,6 +1,8 @@
 //= require "plugins"
-//= require "vendor/fb"
-//= require "vendor/twitter"
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 // $(document).foundation();
+
+// TODO: Decide whether to include any social media scripts
+// require "vendor/fb"
+// require "vendor/twitter"
